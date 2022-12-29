f0b7640b1a6ca01867a6dd0253b2318786f3587a
Mike <mwroberts89@gmail.com>
2022-12-28 20:36:15 -0500

    turned ongoing tasks display into a table and styled it

--- src/OngoingTasks.tsx
+++ src/OngoingTasks.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import Task from './Task'
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md"
+import { IconContext } from 'react-icons/lib/esm/iconContext';
 
 const OngoingTasks = ({ unmutatedTaskList, totalMinutes, minToHours }: { unmutatedTaskList: any, totalMinutes: number, minToHours: Function }) => {
   const [showTasks, setShowTasks] = useState(false);
 
   const totalHours = minToHours(totalMinutes);
 
-  const uploadTxtFile = () => {
-    // convert .txt file to task array, may need to be in the App component
+  const sortTable = (e: any) => {
+    console.log(e.target.textContent);
+    console.log(e.target.innerHTML.split('d="M0 ')[1].split('z"')[0]);
   }
 
   if (showTasks) {
@@ -17,15 +20,27 @@ const OngoingTasks = ({ unmutatedTaskList, totalMinutes, minToHours }: { unmutat
           <h1>Ongoing Tasks</h1>
           <div id='ongoing-tasks-buttons-display'>
             <button onClick={() => setShowTasks(false)}>Hide</button>
-            <div className='pseudo-btn'>import .txt <input type='file' /></div>
           </div>
         </div>
-        <p>{totalHours} hours</p>
-        {
-          unmutatedTaskList.map((item: any) => {
-            return <Task key={item.id} {...item} />
-          })
-        }
+        <p>{totalHours} out of ~72  total non-work/sleep hours per week</p>
+        <IconContext.Provider value={{ className: 'table-icons' }}>
+          <table id="ongoing-tasks-table">
+            <tbody>
+              <tr>
+                <th onClick={sortTable}>Task <MdKeyboardArrowUp /></th>
+                <th onClick={sortTable}>Category <MdKeyboardArrowUp /></th>
+                <th onClick={sortTable}>Times Per Week <MdKeyboardArrowDown /></th>
+                <th onClick={sortTable}>Minutes <MdKeyboardArrowDown /></th>
+              </tr>
+              {
+                unmutatedTaskList.map((item: any) => {
+                  console.log(item);
+                  return <Task key={item.id} {...item} />
+                })
+              }
+            </tbody>
+          </table>
+        </IconContext.Provider>
       </div >
     )
   }
@@ -35,7 +50,7 @@ const OngoingTasks = ({ unmutatedTaskList, totalMinutes, minToHours }: { unmutat
       <h1>Ongoing Tasks</h1>
       <div id='ongoing-tasks-buttons-display'>
         <button onClick={() => setShowTasks(true)}>Show</button>
-        <div className='pseudo-btn'>import .txt <input type='file' /></div>
+        {/* <div className='pseudo-btn'>import .txt <input type='file' /></div> */}
       </div>
     </div>
   )
--- src/Task.tsx
+++ src/Task.tsx
@@ -1,8 +1,12 @@
-const Task = ({ task, timesPerWeek, minEstimate }: { task: string, timesPerWeek: number, minEstimate: number }) => {
+const Task = ({ task, timesPerWeek, minEstimate, categories }: { task: string, timesPerWeek: number, minEstimate: number, categories: Array<string> }) => {
+
   return (
-    <>
-      <div>{timesPerWeek} - {task} - {minEstimate}min</div>
-    </>
+    <tr>
+      <td>{task}</td>
+      <td>{categories[0]}</td>
+      <td>{timesPerWeek}</td>
+      <td>{minEstimate}</td>
+    </tr>
   )
 }
 
